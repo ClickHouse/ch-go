0b7809da7de99c37ac07b5f4c9fa3514d56c2140
Aleksandr Razumov <ernado@ya.ru>
2021-11-16 17:35:25 +0300

    test(proto): update ServerHello test and benchmark

--- internal/proto/server_hello_test.go
+++ internal/proto/server_hello_test.go
@@ -9,17 +9,20 @@ import (
 )
 
 func TestServerHello_Decode(t *testing.T) {
-	data, err := hex.DecodeString("11436c69636b486f75736520736572766572150bb2a903")
+	data, err := hex.DecodeString("11436c69636b486f75736520736572766572150bb2a9030d4575726f70652f4d6f73636f7705616c70686103")
 	require.NoError(t, err)
 
 	r := NewReader(bytes.NewReader(data))
 	var v ServerHello
 	require.NoError(t, v.Decode(r))
 	require.Equal(t, ServerHello{
-		Name:     "ClickHouse server",
-		Major:    21,
-		Minor:    11,
-		Revision: 54450,
+		Name:        "ClickHouse server",
+		Major:       21,
+		Minor:       11,
+		Patch:       3,
+		Revision:    54450,
+		Timezone:    "Europe/Moscow",
+		DisplayName: "alpha",
 	}, v)
 }
 
@@ -29,6 +32,11 @@ func BenchmarkServerHello_Decode(b *testing.B) {
 	raw.PutInt(21)
 	raw.PutInt(11)
 	raw.PutInt(54450)
+	raw.PutString("Europe/Moscow")
+	raw.PutString("alpha")
+	raw.PutInt(3)
+
+	b.Logf("%x", raw.Buf)
 
 	buf := new(bytes.Reader)
 
@@ -70,6 +78,28 @@ func BenchmarkServerHello_Decode(b *testing.B) {
 			revision, err := r.Int()
 			if err != nil {
 				b.Fatal(err)
+			}
+
+			if FeatureTimezone.In(revision) {
+				v, err := r.StrRaw()
+				if err != nil {
+					b.Fatal(err)
+				}
+				_ = v
+			}
+			if FeatureDisplayName.In(revision) {
+				v, err := r.StrRaw()
+				if err != nil {
+					b.Fatal(err)
+				}
+				_ = v
+			}
+			if FeatureVersionPatch.In(revision) {
+				v, err := r.Int()
+				if err != nil {
+					b.Fatal(err)
+				}
+				_ = v
 			}
 
 			_ = name
