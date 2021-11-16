825911a08cc84ceb455c9a1a28bcd5163e4025f9
Aleksandr Razumov <ernado@ya.ru>
2021-11-16 21:14:42 +0300

    feat(proto): rework error formatting

--- internal/proto/error.go
+++ internal/proto/error.go
@@ -1,10 +1,15 @@
 package proto
 
+import "fmt"
+
 // Error on server side.
 type Error int
 
 func (e Error) Error() string {
-	return e.String()
+	if e.IsAError() {
+		return fmt.Sprintf("%s (%d)", e.String(), e)
+	}
+	return fmt.Sprintf("UNKNOWN (%d)", e)
 }
 
 //go:generate go run github.com/dmarkham/enumer -transform snake_upper -type Error -trimprefix Err -output error_gen.go
