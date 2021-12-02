d2feb5a1a10c267bb32b062c3dc5a8f4f2f9d6ab
Aleksandr Razumov <ernado@ya.ru>
2021-12-02 18:25:28 +0300

    test(fuzz): add FuzzQuery_DecodeAware

---
+++ internal/proto/query_fuzz_test.go
@@ -0,0 +1,38 @@
+//go:build go1.18
+
+package proto
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func FuzzQuery_DecodeAware(f *testing.F) {
+	data, err := hex.DecodeString(queryCreateDatabaseHex)
+	require.NoError(f, err)
+
+	f.Add(data)
+	f.Fuzz(func(t *testing.T, data []byte) {
+		var v Query
+		r := NewReader(bytes.NewReader(data))
+		_, _ = r.Uvarint() // skip code
+
+		if err := v.DecodeAware(r, queryProtoVersion); err != nil {
+			t.Skip()
+		}
+
+		b := new(Buffer)
+		v.EncodeAware(b, queryProtoVersion)
+
+		br := NewReader(bytes.NewReader(b.Buf))
+		_, _ = br.Uvarint() // skip code
+
+		var dec Query
+		if err := dec.DecodeAware(br, queryProtoVersion); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
