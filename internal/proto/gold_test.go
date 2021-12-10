23bde465d61d6026dee94cde0e707560ab1a0547
Aleksandr Razumov <ernado@ya.ru>
2021-12-10 13:20:04 +0300

    chore(proto): fix linter issues

--- internal/proto/gold_test.go
+++ internal/proto/gold_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func typeName(v interface{}) string {
-	if t := reflect.TypeOf(v); t.Kind() == reflect.Ptr {
+	t := reflect.TypeOf(v)
+	if t.Kind() == reflect.Ptr {
 		return t.Elem().Name()
-	} else {
-		return t.Name()
 	}
+	return t.Name()
 }
 
 // Gold checks golden version of v encoding.
