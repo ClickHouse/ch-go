f0a7b6b81487d0b0bf8331579b6cdfedd1176860
Aleksandr Razumov <ernado@ya.ru>
2021-12-16 11:27:24 +0300

    feat(proto): add helpers for Date

--- proto/date.go
+++ proto/date.go
@@ -1,6 +1,35 @@
 package proto
 
+import "time"
+
 // Date represents Date value.
 //
 // https://clickhouse.com/docs/en/sql-reference/data-types/date/
 type Date uint16
+
+// DateLayout is default time format for Date.
+const DateLayout = "2006-01-02"
+
+// secInDay represents seconds in day.
+//
+// NB: works only on UTC, use time.Date, time.Time.AddDate.
+const secInDay = 24 * 60 * 60
+
+// Time returns starting time.Time of Date.
+func (d Date) Time() time.Time {
+	return time.Unix(secInDay*int64(d), 0)
+}
+
+func (d Date) String() string {
+	return d.Time().UTC().Format(DateLayout)
+}
+
+// TimeToDate returns Date of time.Time in UTC.
+func TimeToDate(t time.Time) Date {
+	return Date(t.Unix() / secInDay)
+}
+
+// NewDate returns the Date corresponding to year, month and day in UTC.
+func NewDate(year int, month time.Month, day int) Date {
+	return TimeToDate(time.Date(year, month, day, 0, 0, 0, 0, time.UTC))
+}
---
+++ proto/date_test.go
@@ -0,0 +1,44 @@
+package proto
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDate_Time(t *testing.T) {
+	t.Run("Single", func(t *testing.T) {
+		v := time.Date(2011, 10, 10, 14, 59, 31, 401235, time.UTC)
+		d := TimeToDate(v)
+		assert.Equal(t, Date(15257), d)
+		assert.Equal(t, NewDate(2011, 10, 10), d)
+		assert.Equal(t, d.String(), "2011-10-10")
+		assert.Equal(t, d, TimeToDate(d.Time()))
+	})
+	t.Run("Range", func(t *testing.T) {
+		var (
+			start = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+			end   = time.Date(2148, 1, 1, 0, 0, 0, 0, time.UTC)
+		)
+		for v := start; v.Before(end); v = v.AddDate(0, 0, 1) {
+			date := TimeToDate(v)
+			newTime := date.Time()
+			assert.True(t, newTime.Equal(v))
+			newDate := NewDate(newTime.Year(), newTime.Month(), newTime.Day())
+			assert.Equal(t, date, newDate)
+			assert.Equal(t, v.Format("2006-01-02"), date.String())
+		}
+	})
+}
+
+func BenchmarkDate_Time(b *testing.B) {
+	b.ReportAllocs()
+
+	v := Date(100)
+	var t time.Time
+	for i := 0; i < b.N; i++ {
+		t = v.Time()
+	}
+	_ = t.IsZero()
+}
