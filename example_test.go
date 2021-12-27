6e9da150db362ed1e8735c6f3c02afc437c56829
Aleksandr Razumov <ernado@ya.ru>
2021-12-27 13:33:38 +0300

    docs: update ExampleQuery_multipleInputColumns

--- example_test.go
+++ example_test.go
@@ -14,14 +14,19 @@ func ExampleQuery_multipleInputColumns() {
 		name      proto.ColStr
 		sevText   proto.ColEnum8Auto
 		sevNumber proto.ColUInt8
+		arrValues proto.ColStr
+
+		arr = proto.ColArr{Data: &arrValues} // Array(String)
+		now = time.Date(2010, 1, 1, 10, 22, 33, 345678, time.UTC)
 	)
-	now := time.Date(2010, 1, 1, 10, 22, 33, 345678, time.UTC)
+	// Append 10 rows.
 	for i := 0; i < 10; i++ {
 		body.AppendBytes([]byte("Hello"))
 		timestamp = append(timestamp, proto.ToDateTime64(now, proto.PrecisionNano))
 		name.Append("name")
 		sevText.Values = append(sevText.Values, "INFO")
 		sevNumber = append(sevNumber, 10)
+		arrValues.ArrAppend(&arr, []string{"foo", "bar", "baz"})
 	}
 	input := proto.Input{
 		{Name: "timestamp", Data: timestamp.Wrap(proto.PrecisionNano)},
@@ -29,9 +34,10 @@ func ExampleQuery_multipleInputColumns() {
 		{Name: "severity_number", Data: sevNumber},
 		{Name: "body", Data: body},
 		{Name: "name", Data: name},
+		{Name: "arr", Data: arr},
 	}
 	fmt.Println(input.Into("logs"))
 
 	// Output:
-	// INSERT INTO logs (timestamp, severity_text, severity_number, body, name) VALUES
+	// INSERT INTO logs (timestamp, severity_text, severity_number, body, name, arr) VALUES
 }
