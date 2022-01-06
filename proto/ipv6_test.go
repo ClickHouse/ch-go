06119c1d3e45aa41fc1b71e4bc67cc1a222e0534
Aleksandr Razumov <ernado@ya.ru>
2022-01-06 09:22:02 +0300

    feat(proto): improve IPv6 column speed

--- proto/ipv6_test.go
+++ proto/ipv6_test.go
@@ -2,6 +2,7 @@ package proto
 
 import (
 	"bytes"
+	"encoding/binary"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,7 +12,9 @@ import (
 )
 
 func IPv6FromInt(v int) IPv6 {
-	return IPv6(UInt128FromInt(v))
+	s := IPv6{}
+	binary.BigEndian.PutUint64(s[:], uint64(v))
+	return s
 }
 
 func TestColIPv6_NetAddr(t *testing.T) {
