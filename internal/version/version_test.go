3c9d2a588472b8d8d0f680918ba2f9c26282a8f1
Aleksandr Razumov <ernado@ya.ru>
2022-02-06 11:31:54 +0300

    refactor(version): extract and test parsing function

---
+++ internal/version/version_test.go
@@ -0,0 +1,57 @@
+package version
+
+import (
+	"runtime/debug"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestExtract(t *testing.T) {
+	for _, tc := range []struct {
+		Name   string
+		Input  debug.BuildInfo
+		Output Value
+	}{
+		{
+			Name:   "Empty",
+			Output: Value{Name: "dev"},
+		},
+		{
+			Name: "Main",
+			Input: debug.BuildInfo{
+				Main: debug.Module{
+					Path:    "github.com/go-faster/ch/foo/bar",
+					Version: "1.5.10",
+				},
+			},
+			Output: Value{Major: 1, Minor: 5, Patch: 10},
+		},
+		{
+			Name: "Invalid",
+			Input: debug.BuildInfo{
+				Main: debug.Module{
+					Path:    "github.com/go-faster/ch/foo/bar",
+					Version: "bad",
+				},
+			},
+			Output: Value{Name: "dev"},
+		},
+		{
+			Name: "Dependency",
+			Input: debug.BuildInfo{
+				Deps: []*debug.Module{
+					{
+						Path:    "github.com/go-faster/ch",
+						Version: "2.110.145-alpha.0",
+					},
+				},
+			},
+			Output: Value{Major: 2, Minor: 110, Patch: 145, Name: "alpha.0"},
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			require.Equal(t, tc.Output, Extract(&tc.Input))
+		})
+	}
+}
