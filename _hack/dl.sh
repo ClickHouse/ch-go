7a0e7a2532e2a17fdf10fee7ef41393b81709606
Aleksandr Razumov <ernado@ya.ru>
2022-04-07 13:54:15 +0300

    ci(e2e): tar version

--- _hack/dl.sh
+++ _hack/dl.sh
@@ -7,6 +7,7 @@ mkdir -p /opt/ch
 
 URL=$(curl -L -s "https://api.github.com/repos/ClickHouse/ClickHouse/releases/tags/v${1}" | grep -o -P "https:\/\/.*clickhouse-common-static-\d.*\.tgz")
 echo "Downloading ${1} to /opt/ch: ${URL}"
+tar --version
 wget -qO- /tmp/static.tgz "${URL}"| tar -C /opt/ch -v -z --transform 's/\/clickhouse$/clickhouse/' --extract --wildcards "*/bin/clickhouse"
 
 ls -lhsa /opt/ch/clickhouse
