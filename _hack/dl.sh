100c30019aac8c70064b3cb0c10ccac0f6c7bcfd
Aleksandr Razumov <ernado@ya.ru>
2022-04-07 13:26:36 +0300

    ci(e2e): fix grep

--- _hack/dl.sh
+++ _hack/dl.sh
@@ -3,5 +3,5 @@
 # Download static build .tgz by version.
 # Example: _hack/dl.sh 22.3.2.2-lts
 
-URL=$(curl -L -s "https://api.github.com/repos/ClickHouse/ClickHouse/releases/tags/v${1}" | grep -o -E "https:\/\/.*clickhouse-common-static-\d.*\.tgz")
+URL=$(curl -L -s "https://api.github.com/repos/ClickHouse/ClickHouse/releases/tags/v${1}" | grep -o -P "https:\/\/.*clickhouse-common-static-\d.*\.tgz")
 wget -O /tmp/static.tgz "${URL}"
