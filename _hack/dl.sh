81303759141c7c413f542e3fa31c3bb2fc6b9470
Aleksandr Razumov <ernado@ya.ru>
2022-04-07 12:32:54 +0300

    ci(e2e): rework download

---
+++ _hack/dl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download static build .tgz by version.
+# Example: _hack/dl.sh 22.3.2.2-lts
+
+URL=$(curl -L -s "https://api.github.com/repos/ClickHouse/ClickHouse/releases/tags/v${1}" | grep -o -E "https:\/\/.*clickhouse-common-static-\d.*\.tgz")
+wget -O /tmp/static.tgz "${URL}"
