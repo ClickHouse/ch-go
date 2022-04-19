d39d56cfdc804706bfe2996981f32bc2131a19ae
Aleksandr Razumov <ernado@ya.ru>
2022-04-19 22:09:29 +0300

    test(proto): improve ColRawOf test

--- proto/col_raw_of_test.go
+++ proto/col_raw_of_test.go
@@ -2,10 +2,17 @@
 
 package proto
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestColRawOf(t *testing.T) {
 	testColumn[[16]byte](t, "byte_arr_16", func() ColumnOf[[16]byte] {
 		return &ColRawOf[[16]byte]{}
 	}, [16]byte{1: 1}, [16]byte{10: 14})
+
+	require.Equal(t, ColumnType("FixedString(32)"), (ColRawOf[[32]byte]{}).Type())
+	require.Equal(t, ColumnType("FixedString(2)"), (ColRawOf[[2]byte]{}).Type())
 }
