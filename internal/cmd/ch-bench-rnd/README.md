68609322d72745104b21a237bbc133203b96b6e1
Aleksandr Razumov <ernado@ya.ru>
2022-06-29 02:04:52 +0300

    test(bench): rework ch-bench-rnd (use generateRandom, multiple types)

---
+++ internal/cmd/ch-bench-rnd/README.md
@@ -0,0 +1,14 @@
+# Results
+
+
+```
+          String   2.908s   1.0 GB 344 MB/sec  28.66 million rows/sec
+   Array(String)   1.452s   724 MB 499 MB/sec  19.13 million rows/sec
+           UInt8    166ms   500 MB 3.0 GB/sec   3.01 billion rows/sec
+         Float64    633ms   4.0 GB 6.3 GB/sec 789.69 million rows/sec
+        DateTime    347ms   2.0 GB 5.8 GB/sec   1.44 billion rows/sec
+          UInt64    629ms   4.0 GB 6.4 GB/sec 795.15 million rows/sec
+   Array(UInt64)   1.824s    10 GB 5.5 GB/sec 274.18 million rows/sec
+Nullable(UInt64)    903ms   4.5 GB 5.0 GB/sec 553.77 million rows/sec
+            UUID   1.531s   8.0 GB 5.2 GB/sec 326.56 million rows/sec
+```
--- internal/cmd/ch-bench-rnd/main.go
+++ internal/cmd/ch-bench-rnd/main.go
@@ -15,10 +15,25 @@ import (
 	"github.com/ClickHouse/ch-go/proto"
 )
 
+func formatNum(v uint64) string {
+	switch {
+	case v >= 1e9:
+		return fmt.Sprintf("%6.2f billion", float64(v)/1e9)
+	case v >= 1e6:
+		return fmt.Sprintf("%6.2f million", float64(v)/1e6)
+	case v >= 1e3:
+		return fmt.Sprintf("%6.2f thousand", float64(v)/1e3)
+	default:
+		return fmt.Sprintf("%d", v)
+	}
+}
+
 func run(ctx context.Context) error {
 	var arg struct {
 		CPUProfile string
+		Type       string
 	}
+	flag.StringVar(&arg.Type, "type", "", "column type to use")
 	flag.StringVar(&arg.CPUProfile, "cpuprofile", "", "write cpu profile to `file`")
 	flag.Parse()
 
@@ -42,31 +57,65 @@ func run(ctx context.Context) error {
 		_ = c.Close()
 	}()
 
-	var (
-		rows       uint64
-		totalBytes uint64
-		data       proto.ColUInt32
-	)
-	start := time.Now()
-	if err := c.Do(ctx, ch.Query{
-		Body: "SELECT v FROM test_values",
-		OnProgress: func(ctx context.Context, p proto.Progress) error {
-			rows += p.Rows
-			totalBytes += p.Bytes
-			return nil
-		},
-		Result: proto.Results{
-			{Name: "v", Data: &data},
-		},
-	}); err != nil {
-		return errors.Wrap(err, "query")
+	for _, data := range []proto.Column{
+		new(proto.ColStr),
+		new(proto.ColStr).Array(),
+		new(proto.ColUInt8),
+		new(proto.ColFloat64),
+		new(proto.ColDateTime),
+		new(proto.ColUInt64),
+		new(proto.ColUInt64).Array(),
+		new(proto.ColUInt64).Nullable(),
+		new(proto.ColUUID),
+	} {
+		if arg.Type != "" && arg.Type != data.Type().String() {
+			continue
+		}
+		var (
+			rows       uint64
+			totalBytes uint64
+		)
+		const (
+			randSeed  = 1
+			maxStrLen = 6
+			maxArrLen = 3
+			limit     = 500_000_000
+		)
+		start := time.Now()
+		targetLimit := limit
+		switch data.Type() {
+		case "String":
+			targetLimit = limit / maxStrLen
+		case "Array(String)":
+			targetLimit = limit / maxStrLen / maxArrLen
+		}
+		if err := c.Do(ctx, ch.Query{
+			Body: fmt.Sprintf("SELECT v FROM generateRandom('v %s',  %d, %d, %d) LIMIT %d",
+				data.Type(), randSeed, maxStrLen, maxArrLen, targetLimit,
+			),
+			OnResult: func(ctx context.Context, block proto.Block) error {
+				return nil
+			},
+			OnProgress: func(ctx context.Context, p proto.Progress) error {
+				rows += p.Rows
+				totalBytes += p.Bytes
+				return nil
+			},
+			Result: proto.Results{
+				{Name: "v", Data: data},
+			},
+		}); err != nil {
+			return errors.Wrap(err, "query")
+		}
+		duration := time.Since(start)
+		fmt.Printf("%16s %8s %8s %5s %s rows/sec\n",
+			data.Type(),
+			duration.Round(time.Millisecond),
+			humanize.Bytes(totalBytes),
+			humanize.Bytes(uint64(float64(totalBytes)/duration.Seconds()))+"/sec",
+			formatNum(uint64(float64(targetLimit)/duration.Seconds())),
+		)
 	}
-
-	duration := time.Since(start)
-	fmt.Println(duration.Round(time.Millisecond), rows, "rows",
-		humanize.Bytes(totalBytes),
-		humanize.Bytes(uint64(float64(totalBytes)/duration.Seconds()))+"/s",
-	)
 
 	return nil
 }
