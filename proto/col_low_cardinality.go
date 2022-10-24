6765d8932a84882abd5cbd79fb16e860d8dc869f
Aleksandr Razumov <ernado@ya.ru>
2022-10-24 18:46:39 +0300

    fix(proto): reset index in low cardinality

--- proto/col_low_cardinality.go
+++ proto/col_low_cardinality.go
@@ -301,6 +301,7 @@ func (c *ColLowCardinality[T]) Prepare() error {
 	c.keys = append(c.keys[:0], make([]int, len(c.Values))...)
 	if c.kv == nil {
 		c.kv = map[T]int{}
+		c.index.Reset()
 	}
 
 	// Fill keys with value indexes.
